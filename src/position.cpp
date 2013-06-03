720e99499d1236a9da086af95ca31b24008f776f
Ryan Schmitt <ryan.l.schmitt@gmail.com>
2013-06-02 23:43:00 -0700

    Testing version

--- src/position.cpp
+++ src/position.cpp
@@ -294,8 +294,9 @@ void Position::set(const string& fenStr, bool isChess960, Thread* th) {
   st->checkersBB = attackers_to(king_square(sideToMove)) & pieces(~sideToMove);
   chess960 = isChess960;
   thisThread = th;
-
-  assert(pos_is_ok());
+  
+  if(!pos_is_ok()) sync_cout << "info string error bad FEN:" << fenStr << sync_endl;
+  else sync_cout << "info string valid FEN"<<sync_endl;
 }
 
 
@@ -1461,7 +1462,7 @@ bool Position::pos_is_ok(int* failedStep) const {
   int dummy, *step = failedStep ? failedStep : &dummy;
 
   // What features of the position should be verified?
-  const bool all = false;
+  const bool all = true;
 
   const bool debugBitboards       = all || false;
   const bool debugKingCount       = all || false;
